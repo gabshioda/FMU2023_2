c31d3a7ff00fe2272a3d1cfa6736992b58b28051
Gabriel <gabriel_shioda@hotmail.com>
2023-10-11 10:41:38 -0300

    Quarta aula

--- app/src/main/java/com/example/exemplo/MainActivity.java
+++ app/src/main/java/com/example/exemplo/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -47,4 +46,11 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void abrirJanelaDesenho(View view) {
+        Intent intent = new Intent(this, MainActivityDesenho.class);
+        startActivity(intent);
+    }
+
+    public static class DesenhoView {
+    }
 }
---
+++ app/src/main/java/com/example/exemplo/MainActivityDesenho.java
@@ -0,0 +1,14 @@
+package com.example.exemplo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityDesenho extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_desenho);
+    }
+}
